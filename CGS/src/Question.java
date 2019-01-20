e5d16491893feb3a0cf00002a908d2f80ec444e8
Eylam Tagor <eylamtagor@gmail.com>
2019-01-20 14:59:09 -0800

    Add a random class

---
+++ CGS/src/Question.java
@@ -0,0 +1,12 @@
+
+public class Question {
+	private String question, correct, wrong1, wrong2, wrong3;
+
+	public Question(String question, String correct, String wrong1, String wrong2, String wrong3) {
+		this.question = question;
+		this.correct = correct;
+		this.wrong1 = wrong1;
+		this.wrong2 = wrong2;
+		this.wrong3 = wrong3;
+	}
+}
