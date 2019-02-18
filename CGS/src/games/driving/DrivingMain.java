feafe51f9c1b2bb3e4bb559ad7f4f4074d00a9d8
Eylam Tagor <eylamtagor@gmail.com>
2019-02-17 18:29:41 -0800

    Reorganize main classes

--- CGS/src/games/driving/DrivingMain.java
+++ CGS/src/games/driving/DrivingMain.java
@@ -1,18 +1,17 @@
-package general;
+package games.driving;
 
 import javax.swing.JFrame;
 
-import games.space.Space;
-import games.driving.Driving;
+import general.Question;
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
 
-public class Main {
+public class DrivingMain {
 
 	public static void main(String args[]) {
 		// change drawing's type to whatever game you want to run (or duplicate for each
-		// game if you want
-		Space drawing = new Space(new Question("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "right",
+		// game if you want)
+		Driving drawing = new Driving(new Question("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "right",
 				"wrong1", "wrong2", "wrong3"));
 		drawing.setSize(800, 700);
 		PApplet.runSketch(new String[] { "" }, drawing);
---
+++ CGS/src/games/space/SpaceMain.java
@@ -0,0 +1,31 @@
+package games.space;
+
+import javax.swing.JFrame;
+
+import general.Question;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+public class SpaceMain {
+
+	public static void main(String args[]) {
+		// change drawing's type to whatever game you want to run (or duplicate for each
+		// game if you want
+		Space drawing = new Space(new Question("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "right",
+				"wrong1", "wrong2", "wrong3"));
+		drawing.setSize(800, 700);
+		PApplet.runSketch(new String[] { "" }, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame) canvas.getFrame();
+
+		window.setSize(800, 700);
+		window.setLocation(100, 100);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+
+		window.setVisible(true);
+		canvas.requestFocus();
+	}
+
+}
