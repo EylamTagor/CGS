87d81a1a16e99ba650b04a91cf6ea7b517990fc4
Eylam Tagor <eylamtagor@gmail.com>
2019-05-03 20:24:22 -0700

    Update code to recent version, and PACKAGE EVRYTHING FINALLY

---
+++ CGS/src/games/driving/Driving.java
@@ -0,0 +1,312 @@
+package games.driving;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JFrame;
+
+import buttons.AyushTextButton;
+import buttons.TextButton1;
+import other.Player;
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PImage;
+import running.Question;
+
+public class Driving extends PApplet {
+	private Player player;
+	private int conference;
+
+	private Question question;
+	private ArrayList<FuelTank> fuel;
+	private ArrayList<Obstacle> obstacles;
+	private boolean[] gone;
+
+	private PImage car;
+	private PFont font;
+
+	private TextButton1 start, pause, quit;
+
+	private int lane;
+	private double speed;
+
+	private int timer, status; // 0 = running, 1 = hit wrong fuel, 2 = win, 3 = ran out of time, 4 = hit
+								// obstacle, -1 = how to play, -2 = pause
+
+	private JFrame window;
+	private ArrayList<Question> answers, wrongAnswers;
+	private ArrayList<String> rightAnswers;
+	private int index;
+	private AyushTextButton quitintro;
+	private boolean bool;
+
+	public Driving(Question question, Player player, int conference, float speed, ArrayList<Question> answers,
+			ArrayList<Question> wrongAnswers, ArrayList<String> rightAnswers, int index) {
+		this.player = player;
+		this.conference = conference;
+
+		this.question = question;
+		start = new TextButton1(50, 375, 105, 40, 70, 400, 75, 175, 75, 255, 255, 255, "START");
+		pause = new TextButton1(0, 0, 100, 40, 15, 25, 255, 255, 255, 0, 0, 0, "PAUSE");
+		quit = new TextButton1(275, 275, 240, 100, 390, 350, 255, 255, 255, 0, 0, 0, "QUIT");
+
+		fuel = new ArrayList<FuelTank>();
+		obstacles = new ArrayList<Obstacle>();
+		gone = new boolean[4];
+
+		ArrayList<Integer> takenCue = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3)),
+				takenLane = (ArrayList<Integer>) takenCue.clone();
+
+		for (int i = 0; i < 4; i++) {
+			fuel.add(new FuelTank(question, i, takenLane.remove((int) (Math.random() * takenLane.size())),
+					takenCue.remove((int) (Math.random() * takenCue.size()))));
+
+			int obstacleLane = (int) (Math.random() * 4);
+			if (obstacleLane != fuel.get(i).getLane())
+				obstacles.add(new Obstacle(obstacleLane, fuel.get(i).getCue()));
+		}
+
+		status = -1;
+		this.speed = speed;
+		lane = 0;
+		timer = 0;
+
+		this.answers = answers;
+		this.wrongAnswers = wrongAnswers;
+		this.rightAnswers = rightAnswers;
+		this.index = index;
+		quitintro = new AyushTextButton(700, 625, 100, 55, 715, 655, Color.black, Color.white, "Quit", 20);
+
+		bool = false;
+	}
+
+	public void setFrame(JFrame win) {
+		window = win;
+	}
+
+	public void setup() {
+		font = createFont("Arial Bold", 18);
+		car = loadImage("images\\car.png");
+	}
+
+	public void draw() {
+		background(125);
+
+		textFont(font);
+		textAlign(CENTER);
+		textSize(25);
+		fill(255);
+
+		if (status == -2) {
+			textAlign(LEFT);
+			textSize(20);
+			pause.draw(this);
+			if (pause.isPointInButton(mouseX, mouseY))
+				pause.setColor(200, 200, 200);
+			else
+				pause.setColor(255, 255, 255);
+
+			textAlign(CENTER);
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+		} else if (status == -1) {
+			textSize(36);
+			text("EXPERT DRIVING", width / 2, 50);
+			textSize(25);
+			text("How to Play", width / 2, 80);
+			textSize(20);
+			textAlign(LEFT);
+			text("You're about to be late to work, and your fuel is almost gone! On your\nway, you must collect the fuel tanks with the correct answers. Be careful \nnot to hit obstacles or collect fuel tanks with incorrect answers!",
+					50, 125);
+			text("CONTROLS\nUse the left and right arrow keys to switch lanes", 50, 300);
+
+			fill(0);
+			noStroke();
+			start.draw(this);
+			if (start.isPointInButton(mouseX, mouseY))
+				start.setColor(50, 150, 50);
+			else
+				start.setColor(75, 175, 75);
+
+			quitintro.draw(this);
+		} else if (status == 0) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+
+			if (timer == 300) {
+				textAlign(LEFT);
+				pause.draw(this);
+				if (pause.isPointInButton(mouseX, mouseY))
+					pause.setColor(200, 200, 200);
+				else
+					pause.setColor(255, 255, 255);
+
+				textAlign(CENTER);
+
+				int gones = 0;
+
+				for (boolean i : gone)
+					if (i)
+						gones++;
+				if (gones == 4)
+					status = 3;
+
+				strokeWeight(8);
+				stroke(0);
+				line(0, 125, width, 125);
+				line(0, 525, width, 525);
+
+				strokeWeight(5);
+				stroke(244, 226, 66);
+				for (int i = 2; i <= 4; i++)
+					line(0, 100 * i + 25, width, 100 * i + 25);
+				noStroke();
+
+				for (FuelTank tank : fuel) {
+					tank.draw(this);
+
+					if (tank.getX() > -250)
+						tank.move(speed);
+					else
+						gone[fuel.indexOf(tank)] = true;
+
+					if (tank.getLane() == lane && (tank.isPointInside(25) || tank.isPointInside(100)))
+						status = 1;
+				}
+
+				for (Obstacle o : obstacles) {
+					o.draw(this);
+
+					if (o.getX() > -250)
+						o.move(speed);
+
+					if (o.getLane() == lane && (o.isPointInside(25) || o.isPointInside(100)))
+						status = 4;
+				}
+
+				fill(200, 50, 50);
+				image(car, 25, 150 + lane * 100, 75, 50);
+
+				if (fuel.get(0).getLane() == lane && (fuel.get(0).isPointInside(25) || fuel.get(0).isPointInside(100)))
+					status = 2;
+			} else {
+				textSize(36);
+				text("Get Ready! Start in: " + ((int) 5 - timer / 60), width / 2, height / 2);
+				timer++;
+			}
+		} else if (status == 1) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU LOSE!", width / 2, 50);
+			textSize(15);
+			text("You collected the wrong fuel tank. The correct answer is: " + question.getCorrect(), width / 2, 75);
+
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+
+			if (!bool) {
+				bool = true;
+				wrongAnswers.add(answers.get(index));
+			}
+		} else if (status == 2) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU WIN!", width / 2, 50);
+			textSize(15);
+			text("You collected the right fuel tank!", width / 2, 75);
+
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+
+			noLoop();
+			if (!rightAnswers.contains(question.getQuestion())) {
+				player.passGame(conference);
+			}
+
+			rightAnswers.add(question.getQuestion());
+		} else if (status == 3) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU LOSE!", width / 2, 50);
+			textSize(15);
+			text("You were too slow, and missed all the fuel tanks!", width / 2, 75);
+
+			textSize(60);
+			quit.draw(this);
+
+			if (bool) {
+				bool = true;
+				wrongAnswers.add(answers.get(index));
+			}
+		} else if (status == 4) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU LOSE!", width / 2, 50);
+			textSize(15);
+			text("You crashed into an obstacle!", width / 2, 75);
+
+			textSize(60);
+			quit.draw(this);
+
+			if (bool) {
+				bool = true;
+				wrongAnswers.add(answers.get(index));
+			}
+		}
+	}
+
+	public void mouseClicked() {
+		if (status == -1 && start.isInBounds(mouseX, mouseY))
+			status = 0;
+
+		if (pause.isInBounds(mouseX, mouseY)) {
+			if (status == 0 && timer == 300) {
+				status = -2;
+				pause.setText("RESUME");
+				pause.setWidth(115);
+			} else if (status == -2) {
+				status = 0;
+				pause.setText("PAUSE");
+				pause.setWidth(100);
+			}
+		}
+
+		if ((status == -2 || status == 1 || status == 2 || status == 3 || status == 4)
+				&& quit.isInBounds(mouseX, mouseY))
+			window.dispose();
+
+		if (quitintro.isPointInButton(mouseX, mouseY)) {
+			window.dispose();
+		}
+	}
+
+	public void mouseMoved() {
+		if (quitintro.isPointInButton(mouseX, mouseY)) {
+			quitintro.setBColor(new Color(79, 79, 79));
+		} else {
+			quitintro.setBColor(Color.WHITE);
+		}
+	}
+
+	public void keyPressed() {
+		if (key == CODED) {
+			if (keyCode == UP && lane > 0)
+				lane--;
+			if (keyCode == DOWN && lane < 3)
+				lane++;
+		}
+	}
+}
---
+++ CGS/src/games/space/Space.java
@@ -0,0 +1,304 @@
+package games.space;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import buttons.AyushTextButton;
+import buttons.TextButton1;
+import other.Player;
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PImage;
+import running.Question;
+
+public class Space extends PApplet {
+	private Player player;
+	private int conference;
+
+	private Question question;
+
+	private PImage background, astronaut;
+	private PFont font;
+
+	private TextButton1 start, pause, quit;
+
+	private Asteroid[] asteroids;
+	public static final float[] randomVals = { 100, 250, 400, 550 };
+	private boolean[] gone;
+
+	private float playerX, playerY;
+	private boolean up, down, left, right;
+	private int timer, status; // 0 = running, 1 = lose, 2 = win, -1 = how to play, -2 = pause
+	private double speed;
+
+	private JFrame window;
+	private AyushTextButton quitintro;
+	private ArrayList<Question> answers, wrongAnswers;
+	private ArrayList<String> rightAnswers;
+	private int index;
+
+	private boolean bool;
+
+	public Space(Question question, Player player, int conference, float speed, ArrayList<Question> answers,
+			ArrayList<Question> wrongAnswers, ArrayList<String> rightAnswers, int index) {
+		this.player = player;
+		this.conference = conference;
+		this.question = question;
+		start = new TextButton1(50, 375, 105, 40, 70, 400, 255, 255, 255, 0, 0, 0, "START");
+		pause = new TextButton1(0, 0, 100, 40, 15, 25, 255, 255, 255, 0, 0, 0, "PAUSE");
+		quit = new TextButton1(275, 275, 240, 100, 390, 350, 255, 255, 255, 0, 0, 0, "QUIT");
+
+		asteroids = new Asteroid[4];
+
+		for (int i = 0; i < asteroids.length; i++) {
+			asteroids[i] = new Asteroid(question, i, 0, randomVals[(int) (Math.random() * 4)]);
+		}
+
+		boolean[] taken = new boolean[4];
+		int count = 0;
+		while (!taken[0] || !taken[1] || !taken[2] || !taken[3]) {
+			int ind = (int) (Math.random() * 4);
+			if (!taken[ind]) {
+				asteroids[ind].moveTo(200 * count + 100, asteroids[ind].getY());
+				taken[ind] = true;
+				count++;
+			}
+		}
+
+		playerX = 400;
+		playerY = 350;
+
+		up = false;
+		down = false;
+		left = false;
+		right = false;
+		gone = new boolean[4];
+		status = -1;
+		this.speed = speed;
+		timer = 0;
+
+		this.answers = answers;
+		this.wrongAnswers = wrongAnswers;
+		this.rightAnswers = rightAnswers;
+		this.index = index;
+		quitintro = new AyushTextButton(700, 625, 100, 55, 715, 655, Color.black, Color.white, "Quit", 20);
+
+		bool = false;
+	}
+
+	public void setFrame(JFrame win) {
+		window = win;
+	}
+
+	public void setup() {
+		background = loadImage("images\\Space-800x700.jpg");
+		astronaut = loadImage("images\\astronaut-clipart-png.png");
+		font = createFont("Arial Bold", 18);
+	}
+
+	public void draw() {
+		background(255);
+		image(background, 0, 0, 800, 700);
+
+		textFont(font);
+		textAlign(CENTER);
+		textSize(25);
+		fill(255);
+
+		if (status == -2) {
+			textAlign(LEFT);
+			textSize(20);
+			pause.draw(this);
+			if (pause.isPointInButton(mouseX, mouseY))
+				pause.setColor(200, 200, 200);
+			else
+				pause.setColor(255, 255, 255);
+
+			textAlign(CENTER);
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+		} else if (status == -1) {
+			textSize(36);
+			text("SPACE EXPLORATION", width / 2, 50);
+			textSize(25);
+			text("How to Play", width / 2, 80);
+			textSize(20);
+			textAlign(LEFT);
+			text("An astronaut is on a mission to explore an asteroid. As he approaches\nthe asteroid, the astronaut realizes there are four asteroids, each with\na different answer to your question. Help advance humanity by guiding\nthe astronaut onto the correct asteroid before they drift off the screen!",
+					50, 125);
+			text("CONTROLS\nUse the arrow keys to navigate through space", 50, 300);
+
+			fill(0);
+			start.draw(this);
+			quitintro.draw(this);
+
+			if (start.isPointInButton(mouseX, mouseY))
+				start.setColor(200, 200, 200);
+			else
+				start.setColor(255, 255, 255);
+		} else if (status == 0) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			if (timer == 300) {
+				textAlign(LEFT);
+				pause.draw(this);
+				if (pause.isPointInButton(mouseX, mouseY))
+					pause.setColor(200, 200, 200);
+				else
+					pause.setColor(255, 255, 255);
+				textAlign(CENTER);
+
+				int gones = 0;
+				for (boolean b : gone)
+					if (b)
+						gones++;
+
+				if (gones == 4)
+					status = 1;
+
+				for (int i = 0; i < asteroids.length; i++) {
+					asteroids[i].draw(this);
+
+					if (asteroids[i].getY() < 0 || asteroids[i].getY() > height)
+						gone[i] = true;
+
+					if (asteroids[i].isTop())
+						asteroids[i].moveTo(asteroids[i].getX(), (float) (asteroids[i].getY() + speed));
+					else
+						asteroids[i].moveTo(asteroids[i].getX(), (float) (asteroids[i].getY() - speed));
+				}
+
+				if (asteroids[0].isEllipseInside(playerX + 25, playerY + 25, 50, 50))
+					status = 2;
+
+				for (int i = 1; i < asteroids.length; i++) {
+					if (asteroids[i].isEllipseInside(playerX + 25, playerY + 25, 50, 50))
+						status = 1;
+				}
+
+				image(astronaut, playerX, playerY, 50, 50);
+
+				if (up && playerY > 0)
+					playerY -= 7.5;
+				if (down && playerY < 625)
+					playerY += 7.5;
+				if (left && playerX > 0)
+					playerX -= 7.5;
+				if (right && playerX < 745)
+					playerX += 7.5;
+			} else {
+				textSize(36);
+				text("Get Ready! Start in: " + ((int) 5 - timer / 60), width / 2, height / 2);
+				timer++;
+			}
+		} else if (status == 1) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU LOSE!", width / 2, 50);
+			textSize(15);
+			text("Either time's up, or you landed on the\nwrong asteroid. The correct answer is: "
+					+ question.getCorrect(), width / 2, 75);
+			textAlign(CENTER);
+
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+
+			if (!bool) {
+				bool = true;
+				wrongAnswers.add(answers.get(index));
+			}
+		} else if (status == 2) {
+			textSize(20);
+			text("QUESTION: " + question.getQuestion(), width / 2, height - 75);
+			text("YOU WIN!", width / 2, 50);
+			textSize(15);
+			text("You guided the astronaut\nonto the correct asteroid!", width / 2, 75);
+			textAlign(CENTER);
+
+			textSize(60);
+			quit.draw(this);
+			if (quit.isPointInButton(mouseX, mouseY))
+				quit.setColor(200, 200, 200);
+			else
+				quit.setColor(255, 255, 255);
+
+			noLoop();
+			if (!rightAnswers.contains(question.getQuestion())) {
+				player.passGame(conference);
+			}
+
+			rightAnswers.add(question.getQuestion());
+		}
+	}
+
+	public void mouseClicked() {
+		if (status == -1 && start.isInBounds(mouseX, mouseY))
+			status = 0;
+
+		if (pause.isInBounds(mouseX, mouseY)) {
+			if (status == 0 && timer == 300) {
+				status = -2;
+				pause.setText("RESUME");
+				pause.setWidth(115);
+			} else if (status == -2) {
+				status = 0;
+				pause.setText("PAUSE");
+				pause.setWidth(100);
+			}
+		}
+
+		if (quitintro.isPointInButton(mouseX, mouseY)) {
+			window.dispose();
+		}
+
+		if ((status == -2 || status == 1 || status == 2) && quit.isInBounds(mouseX, mouseY))
+			window.dispose();
+	}
+
+	public void mouseMoved() {
+		if (quitintro.isPointInButton(mouseX, mouseY)) {
+			quitintro.setBColor(new Color(125, 125, 125));
+		} else {
+			quitintro.setBColor(Color.white);
+		}
+	}
+
+	public void keyPressed() {
+		if (key == CODED) {
+			if (keyCode == UP)
+				up = true;
+			if (keyCode == DOWN)
+				down = true;
+			if (keyCode == LEFT)
+				left = true;
+			if (keyCode == RIGHT)
+				right = true;
+
+			if (keyCode == ESC)
+				window.dispose();
+		}
+	}
+
+	public void keyReleased() {
+		if (key == CODED) {
+			if (keyCode == UP)
+				up = false;
+			if (keyCode == DOWN)
+				down = false;
+			if (keyCode == LEFT)
+				left = false;
+			if (keyCode == RIGHT)
+				right = false;
+		}
+	}
+}
